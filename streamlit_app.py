688e5848731673086187466d310841ee8a2456b7
Gayathri-kandukuri <109798291+Gayathri-kandukuri@users.noreply.github.com>
2022-07-22 16:23:16 +0530

    Added Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.text('Kale,Spinach and Rocket Smoothie')
+streamlit.text('Hard_boiled Free_range Egg)
